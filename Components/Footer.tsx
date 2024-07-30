3233d650d1825e985e0b23d9cdf2c4a4647a9362
Deepankershamra1999 <Deepankersharma06@gmail.com>
2024-07-30 13:06:22 +0530

    footer updated

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -36,43 +36,43 @@ const Footer = () => {
 
                 <div className="lg:w-1/4 md:w-1/2 w-full px-4">
                   <h2 className="title-font font-bold text-gray-900 tracking-widest text-[20px] lg:text-[20px]">Company</h2>
-                  <nav className="leading-10 mt-8">
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">About us</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Home</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Pricing</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Demo</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Contact us</a></p>
+                  <nav className="mt-8">
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800">About us</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800">Home</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800">Pricing</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800">Demo</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800">Contact us</a></p>
                   </nav>
                 </div>
 
 
                 <div className="lg:w-1/4 md:w-1/2 w-full px-4">
                   <h2 className="title-font font-bold text-gray-900 tracking-widest text-[20px] lg:text-[20px]">Support Us</h2>
-                  <nav className="leading-10 mt-8">
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Blog</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Help center</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">FAQ&apos;s</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Product</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Updates</a></p>
+                  <nav className=" mt-8">
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800 mt-2">Blog</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800 mt-2">Help center</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800 mt-2">FAQ&apos;s</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800 mt-2">Product</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800 mt-2">Updates</a></p>
                   </nav>
                 </div>
 
 
                 <div className="lg:w-1/4 md:w-1/2 w-full px-4">
                   <h2 className="title-font font-bold text-gray-900 tracking-widest text-[20px] lg:text-[20px]">Resources</h2>
-                  <nav className="leading-10 mt-8">
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Community</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Contact</a></p>
-                    <p><a href="" className="text-gray-600 hover:text-gray-800">Terms of service</a></p>
+                  <nav className=" mt-8">
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800 mt-2">Community</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800 mt-2">Contact</a></p>
+                    <p className='mt-5'><a href="" className="text-gray-600 hover:text-gray-800 mt-2">Terms of service</a></p>
                   </nav>
                 </div>
 
 
                 <div className="lg:w-1/4 md:w-1/2 w-full px-4">
                   <h2 className="title-font font-bold text-gray-900 tracking-widest text-[20px] lg:text-[20px]">Address</h2>
-                  <div className="leading-10 mt-8">
-                  <p className="text-gray-600">101 Marlow Street, #12-05 Clife Parkview, Singapore 059020. View on Maps</p>
-                  <p className="text-gray-600">Inquiries: +65 6156 5519 hello@Newsify.com</p>
+                  <div className=" mt-8">
+                  <p className="text-gray-600 mt-2">101 Marlow Street, #12-05 Clife Parkview, Singapore 059020. View on Maps</p>
+                  <p className="text-gray-600 mt-2">Inquiries: +65 6156 5519 hello@Newsify.com</p>
                   <div className="mt-4 flex justify-center md:justify-start gap-5">
                     <a href="" className="text-gray-600 hover:text-gray-800 h-20 w-20"><FaTwitter /></a>
                     <a href="" className="text-gray-600 hover:text-gray-800 h-20 w-20"><FaFacebookF /></a>
